4729c39a8ac1472cfcb9ff18b1fb4a1bc7993757
MuhammadAli13562 <m.ali8.aj8@gmail.com>
2024-04-17 07:36:16 +0500

    hardcoded env due to error

--- apps/client/src/redux/api/index.ts
+++ apps/client/src/redux/api/index.ts
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { ServerEndpoint } from "src/constants"
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: ServerEndpoint }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://chatapp.ali-98-ec2-backend.click" }),
   endpoints: () => ({}),
 })
