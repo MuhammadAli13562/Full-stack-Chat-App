575ab74e2ab94895d3b23101ce389bf0a040302b
MuhammadAli13562 <m.ali8.aj8@gmail.com>
2024-04-22 09:07:13 +0500

    backend api modded

--- apps/client/src/constants/index.ts
+++ apps/client/src/constants/index.ts
@@ -1,5 +1,6 @@
 export const fixedCacheKey = import.meta.env.VITE_FIXED_CACHE_KEY
-export const ServerEndpoint = import.meta.env.VITE_SERVER_ENDPOINT
+//export const ServerEndpoint = import.meta.env.VITE_SERVER_ENDPOINT
+export const ServerEndpoint = "https://chatapp.ali-98-ec2-backend.click/"
 
 export const SideAnimationOpen = [{ x: 0 }, { duration: 0.2 }]
 export const SideAnimationExit = [{ x: -500 }, { duration: 0.2 }]
