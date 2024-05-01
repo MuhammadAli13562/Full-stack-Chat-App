89e956a47d3edb65a4abf82956f544ac52528111
MuhammadAli13562 <m.ali8.aj8@gmail.com>
2024-05-01 09:35:34 +0500

    check foo

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -8,7 +8,6 @@ import Home from "./_root/pages/Home"
 import { AuthLayout } from "./_auth/AuthLayout"
 import SignInForm from "./_auth/forms/SignInForm"
 import SignUpForm from "./_auth/forms/SignUpForm"
-import Request from "./_root/pages/Request"
 
 const App = () => {
   return (
@@ -25,7 +24,6 @@ const App = () => {
             {/* Private Routes */}
             <Route element={<RootLayout />}>
               <Route index element={<Home />} />
-              <Route path="/request" element={<Request />} />
             </Route>
           </Routes>
         </Router>
